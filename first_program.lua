fc0527736cdb3b4fd505e79ec2a4aa40058298b2
Repsak27 <124000508+Repsak27@users.noreply.github.com>
2025-03-20 15:42:30 -0400

    update first_program.lua

--- first_program.lua
+++ first_program.lua
@@ -9,7 +9,47 @@ local num5 = 5
 
 local maxnum = math.max(num1, num2, num3, num4, num5)
 
-print("1. The highest score in this game is " .. maxnum .. "! Can you beat it?" )
+print("1. The highest name in this game is " .. maxnum .. "! Can you beat it?" )
 
 math.randomseed(os.time())
 print("2. Did you guess " .. math.random(1, 100) .. "? If so, congradulations!")
+
+-- Data type - string
+local message = "3. Get ready to have the worst experience of your life!"
+
+print(string.upper(message))
+
+local newMessage1 = string.gsub(message, "worst", "best")
+local newMessage2 = string.gsub(newMessage1, "3", "4")
+
+print(newMessage2)
+
+-- Data type - boolean
+local hasWon = true
+hasWon = false
+
+if hasWon then
+    print("Congradulations!!")
+else
+    print("5. You've lost this round, good luck next time.")
+end
+
+local isAdult = true
+local isSmart = false
+
+if isAdult or isSmart then
+    print("6. Welcome to Lua Warriors where we'll make you feel smarter!")
+end
+
+-- Data type - nil
+local jackpot = nil
+
+if not jackpot then
+    print("7. Oh no, you didn't win the jackpot.")
+end
+
+local name
+
+if not name then
+    print("8. You forgot to type your name!")
+end
